a925626e7e4ba28effe173eceb30ae4202111030
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2018-06-26 07:11:55 +0900

    Fix a hinge loss

--- main.py
+++ main.py
@@ -9,20 +9,15 @@ import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torchnet as tnt
 from sklearn.datasets.samples_generator import make_blobs
-from torch.autograd import Variable
 
 from model import LinearSVM
 
 
 def get_data():
-    X, Y = make_blobs(n_samples=500, centers=2,
-                      random_state=0, cluster_std=0.4)
+    X, Y = make_blobs(n_samples=500, centers=2, random_state=0, cluster_std=0.4)
     return X, Y
 
 
@@ -43,26 +38,24 @@ def train(X, Y, model, args):
         sum_loss = 0
 
         for i in range(0, N, args.batchsize):
-            x = X[perm[i:i + args.batchsize]]
-            y = Y[perm[i:i + args.batchsize]]
+            x = X[perm[i : i + args.batchsize]]
+            y = Y[perm[i : i + args.batchsize]]
 
             if torch.cuda.is_available():
                 x = x.cuda()
                 y = y.cuda()
-            x = Variable(x)
-            y = Variable(y)
 
             optimizer.zero_grad()
             output = model(x)
-            loss = torch.mean(torch.clamp(1 - output * y, min=0))  # hinge loss
-            loss += args.c * torch.mean(model.fc.weight**2)  # l2 penalty
+
+            loss = torch.mean(torch.clamp(1 - output.t() * y, min=0))  # hinge loss
+            loss += args.c * torch.mean(model.fc.weight ** 2)  # l2 penalty
             loss.backward()
             optimizer.step()
 
-            sum_loss += to_np(loss)[0]
+            sum_loss += to_np(loss)
 
-        if epoch % 1 == 0:
-            print('Epoch:{:4d}\tloss:{}'.format(epoch, sum_loss / N))
+        print("Epoch:{:4d}\tloss:{}".format(epoch, sum_loss / N))
 
 
 def visualize(X, Y, model):
@@ -73,7 +66,7 @@ def visualize(X, Y, model):
     x = np.arange(X[:, 0].min(), X[:, 0].max(), delta)
     y = np.arange(X[:, 1].min(), X[:, 1].max(), delta)
     x, y = np.meshgrid(x, y)
-    xy = map(np.ravel, [x, y])
+    xy = list(map(np.ravel, [x, y]))
 
     z = (W.dot(xy) + b).reshape(x.shape)
     z[np.where(z > 1.)] = 4
@@ -84,10 +77,8 @@ def visualize(X, Y, model):
     plt.figure(figsize=(10, 10))
     plt.xlim([X[:, 0].min() + delta, X[:, 0].max() - delta])
     plt.ylim([X[:, 1].min() + delta, X[:, 1].max() - delta])
-    plt.xticks([])
-    plt.yticks([])
-    plt.contourf(x, y, z, alpha=0.8)
-    plt.scatter(x=X[:, 0], y=X[:, 1], c='black', s=10)
+    plt.contourf(x, y, z, alpha=0.8, cmap="Greys")
+    plt.scatter(x=X[:, 0], y=X[:, 1], c="black", s=10)
     plt.tight_layout()
     plt.show()
 
@@ -105,12 +96,12 @@ def main(args):
     visualize(X, Y, model)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--c', type=float, default=0.01)
-    parser.add_argument('--lr', type=float, default=0.1)
-    parser.add_argument('--batchsize', type=int, default=1)
-    parser.add_argument('--epoch', type=int, default=10)
+    parser.add_argument("--c", type=float, default=0.01)
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--batchsize", type=int, default=1)
+    parser.add_argument("--epoch", type=int, default=10)
     args = parser.parse_args()
 
     main(args)
