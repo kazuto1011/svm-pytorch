0209ad09bc6739d6dca9abb096c8bcc5783fa5f0
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2020-01-22 06:30:11 +0900

    Fix a L2 penalty part

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 * Support Vector Machines (SVMs) with Linear Kernel
 * Stochastic Gradient Descent (SGD)
 
-## Dependencies
-PyTorch 0.4.0
+## Requirements
+
+```
+$ pip install matplotlib numpy scikit-learn
+$ pip install "torch>=0.4.0"
+```
 
 ## Example
 
@@ -12,6 +16,6 @@ PyTorch 0.4.0
 python main.py --c 0.01 --batchsize 1
 ```
 
-|Hard margin<br>c = 0, batchsize = 1|Soft margin<br>c = 0.01, batchsize = 1|
-|:-:|:-:|
-|![](./sample/hardmargin.png)|![](./sample/softmargin.png)|
+| Hard margin<br>c = 0, batchsize = 1 | Soft margin<br>c = 0.01, batchsize = 1 |
+| :---------------------------------: | :------------------------------------: |
+|     ![](./docs/hardmargin.png)      |       ![](./docs/softmargin.png)       |
--- main.py
+++ main.py
@@ -10,19 +10,9 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.optim as optim
 from sklearn.datasets.samples_generator import make_blobs
-
-from model import LinearSVM
-
-
-def get_data():
-    X, Y = make_blobs(n_samples=500, centers=2, random_state=0, cluster_std=0.4)
-    return X, Y
-
-
-def to_np(x):
-    return x.data.cpu().numpy()
 
 
 def train(X, Y, model, args):
@@ -38,41 +28,39 @@ def train(X, Y, model, args):
         sum_loss = 0
 
         for i in range(0, N, args.batchsize):
-            x = X[perm[i : i + args.batchsize]]
-            y = Y[perm[i : i + args.batchsize]]
-
-            if torch.cuda.is_available():
-                x = x.cuda()
-                y = y.cuda()
+            x = X[perm[i : i + args.batchsize]].to(args.device)
+            y = Y[perm[i : i + args.batchsize]].to(args.device)
 
             optimizer.zero_grad()
-            output = model(x)
+            output = model(x).squeeze()
+            weight = model.weight.squeeze()
 
-            loss = torch.mean(torch.clamp(1 - output.t() * y, min=0))  # hinge loss
-            loss += args.c * torch.mean(model.fc.weight ** 2)  # l2 penalty
+            loss = torch.mean(torch.clamp(1 - y * output, min=0))
+            loss += args.c * (weight.t() @ weight) / 2.0
+
             loss.backward()
             optimizer.step()
 
-            sum_loss += to_np(loss)
+            sum_loss += float(loss)
 
-        print("Epoch:{:4d}\tloss:{}".format(epoch, sum_loss / N))
+        print("Epoch: {:4d}\tloss: {}".format(epoch, sum_loss / N))
 
 
 def visualize(X, Y, model):
-    W = to_np(model.fc.weight[0])
-    b = to_np(model.fc.bias[0])
+    W = model.weight.squeeze().detach().cpu().numpy()
+    b = model.bias.squeeze().detach().cpu().numpy()
 
-    delta = 0.01
+    delta = 0.001
     x = np.arange(X[:, 0].min(), X[:, 0].max(), delta)
     y = np.arange(X[:, 1].min(), X[:, 1].max(), delta)
     x, y = np.meshgrid(x, y)
     xy = list(map(np.ravel, [x, y]))
 
     z = (W.dot(xy) + b).reshape(x.shape)
-    z[np.where(z > 1.)] = 4
-    z[np.where((z > 0.) & (z <= 1.))] = 3
-    z[np.where((z > -1.) & (z <= 0.))] = 2
-    z[np.where(z <= -1.)] = 1
+    z[np.where(z > 1.0)] = 4
+    z[np.where((z > 0.0) & (z <= 1.0))] = 3
+    z[np.where((z > -1.0) & (z <= 0.0))] = 2
+    z[np.where(z <= -1.0)] = 1
 
     plt.figure(figsize=(10, 10))
     plt.xlim([X[:, 0].min() + delta, X[:, 0].max() - delta])
@@ -83,25 +71,24 @@ def visualize(X, Y, model):
     plt.show()
 
 
-def main(args):
-    X, Y = get_data()
-    X = (X - X.mean()) / X.std()
-    Y[np.where(Y == 0)] = -1
-
-    model = LinearSVM()
-    if torch.cuda.is_available():
-        model.cuda()
-
-    train(X, Y, model, args)
-    visualize(X, Y, model)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--c", type=float, default=0.01)
     parser.add_argument("--lr", type=float, default=0.1)
-    parser.add_argument("--batchsize", type=int, default=1)
+    parser.add_argument("--batchsize", type=int, default=5)
     parser.add_argument("--epoch", type=int, default=10)
+    parser.add_argument("--device", default="cuda", choices=["cpu", "cuda"])
     args = parser.parse_args()
+    args.device = torch.device(args.device if torch.cuda.is_available() else "cpu")
 
-    main(args)
+    print(args)
+
+    X, Y = make_blobs(n_samples=500, centers=2, random_state=0, cluster_std=0.4)
+    X = (X - X.mean()) / X.std()
+    Y[np.where(Y == 0)] = -1
+
+    model = nn.Linear(2, 1)
+    model.to(args.device)
+
+    train(X, Y, model, args)
+    visualize(X, Y, model)
